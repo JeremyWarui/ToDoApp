d8508df9e58a48ab4975a35aa3de89faa081dc89
Jeremy <jerywarne7@gmail.com>
2022-06-25 23:14:08 +0300

    dark and light theme mode

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 "use strict";
-const checkBoxes = document.querySelectorAll("input");
+const checkBoxes = document.querySelectorAll(".check-box");
+const body = document.querySelector("body");
+const taskContainer = document.querySelector(".task-container");
+const toggleIcon = document.querySelector("#toggleIcon");
+const headerContain = document.querySelector("header");
+// const headerImage = document.querySelector("");
 
 // checkbox to add a strikethrough on the list
 for (const checkBox of checkBoxes) {
@@ -7,4 +12,24 @@ for (const checkBox of checkBoxes) {
     const itemSelected = checkBox.parentElement.nextElementSibling;
     itemSelected.classList.toggle("checked-item");
   });
+}
+
+// change the theme of the app whe toggle icon is clicked
+// dark mode
+/*
+1. toggle icon when clicked change the icon to sun icon
+2. change background color f the body and task container
+*/
+
+toggleIcon.addEventListener("click", changeTheme);
+
+function changeTheme() {
+  body.classList.toggle("dark");
+  taskContainer.classList.toggle("dark");
+
+  // change icon
+  toggleIcon.src = "./images/icon-sun.svg";
+  if (!body.classList.contains("dark")) {
+    toggleIcon.src = "./images/icon-moon.svg";
+  }
 }
